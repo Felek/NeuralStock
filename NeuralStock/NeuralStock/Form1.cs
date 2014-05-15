d9af4f21c62fe5fd289067a6542c7c23151394cb
Felek <tomguz@o2.pl>
2014-05-16 00:57:23 +0200

    Pierwszy commit

---
+++ NeuralStock/NeuralStock/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NeuralStock
+{
+    public partial class Form1 : Form
+    {
+        NeuralNetwork network;
+
+        public Form1()
+        {
+            InitializeComponent();
+            network = new NeuralNetwork();
+            network.Teach();
+            textBox1.Text = network.Think(new double[] { 0, 0 })[0].ToString();
+            textBox2.Text = network.Think(new double[] { 0, 1 })[0].ToString();
+            textBox3.Text = network.Think(new double[] { 1, 0 })[0].ToString();
+            textBox4.Text = network.Think(new double[] { 1, 1 })[0].ToString();
+        }
+    }
+}
---
+++ NeuralStock/NeuralStock/NeuralNetwork.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AForge.Neuro;
+using AForge.Neuro.Learning;
+
+namespace NeuralStock
+{
+    class NeuralNetwork
+    {
+        private ActivationNetwork network;
+        private BackPropagationLearning teacher;
+
+        public NeuralNetwork()
+        {
+            network = new ActivationNetwork(new SigmoidFunction(2), 2, 2, 1);
+            teacher = new BackPropagationLearning(network);
+        }
+
+        public void Teach()
+        {
+            double[][] input = new double[4][] {
+                new double[] {0, 0}, new double[] {0, 1},
+                new double[] {1, 0}, new double[] {1, 1}
+            };
+            double[][] output = new double[4][] {
+                new double[] {0}, new double[] {1},
+                new double[] {1}, new double[] {0}
+            };
+
+            bool needToStop = false;
+
+            while (!needToStop)
+            {
+                double error = teacher.RunEpoch(input, output);
+                // check error value to see if we need to stop
+                // ...
+                if (error < 0.01)
+                {
+                    needToStop = true;
+                }
+            }  
+        }
+
+        public double[] Think(double[] input)
+        {
+            return network.Compute(input);
+        }
+    }
+}
